6c2b362c48d9950e318f0a4ed227e94b4fbb0ba3
pzy <1012839072@qq.com>
2019-07-19 14:50:56 +0800

    简单的注释说明 完善构造部分

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,10 +3,18 @@ const Luck = require("./luck.js");
 const uilt = require('../../utils/util.js');
 Page({
   data: {
-    width: '',
-    height: ''
+    width: 300,
+    height: 200,
   },
   onLoad() {
-    this.luck = new Luck(this);
+    let options = {
+      canvasId:'luck',
+      width: this.data.width,//canvas宽
+      height: this.data.height,//canvas高
+      maskColor: '#dddddd',//遮罩的颜色
+      size:10,//清除轨迹的宽度
+      scale:0.75,//可以理解为手动清除面积上限，范围0~1
+    }
+    this.luck = new Luck(this, options);
   }
 })
--- src/pages/index/luck.js
+++ src/pages/index/luck.js
@@ -1,46 +1,48 @@
 class Luck {
-  constructor(page, opts) {
-    opts = opts || {};
+  //构造器
+  constructor(page, options) {
     this.page = page;
-    this.canvasId = opts.canvasId || 'luck';
-    this.width = opts.width || 300;
-    this.height = opts.height || 150;
-    this.maskColor = opts.maskColor || '#dddddd';
-    this.size = opts.size || 8;
+    this.canvasId = options.canvasId ;
+    this.width = options.width;
+    this.height = options.height;
+    this.maskColor = options.maskColor;
+    this.size = options.size;
     this.r = this.size * 2;
     this.area = this.r * this.r;
-    this.scale = opts.scale || 0.75;
+    this.scale = options.scale;
     this.totalArea = this.width * this.height;
-
     this.init();
   }
+  //初始化
   init() {
     this.show = false;
-    this.clearPoints = [];
+    this.clearPointsList = [];//清除的坐标点
     this.ctx = wx.createCanvasContext(this.canvasId, this);
     this.drawMask();
     this.bindTouch();
   }
+  //创建遮罩
   drawMask() {
     this.ctx.setFillStyle(this.maskColor);
     this.ctx.fillRect(0, 0, this.width, this.height);
     this.ctx.draw();
   }
+  //清除操作
   eraser(e, bool) {
-    let len = this.clearPoints.length;
+    let len = this.clearPointsList.length;
     let count = 0
     let x = e.touches[0].x, y = e.touches[0].y;
     let x1 = x - this.size;
     let y1 = y - this.size;
     if (bool) {
-      this.clearPoints.push({
+      this.clearPointsList.push({
         x1: x1,
         y1: y1,
         x2: x1 + this.r,
         y2: y1 + this.r
       })
     }
-    for (let val of this.clearPoints) {
+    for (let val of this.clearPointsList) {
       if (val.x1 > x || val.y1 > y || val.x2 < x || val.y2 < y) {
         count++;
       } else {
@@ -48,19 +50,20 @@ class Luck {
       }
     }
     if (len === count) {
-      this.clearPoints.push({
+      this.clearPointsList.push({
         x1: x1,
         y1: y1,
         x2: x1 + this.r,
         y2: y1 + this.r
       })
     }
-    if (this.clearPoints.length && this.r * this.r * this.clearPoints.length > this.scale * this.totalArea) {
+    if (this.clearPointsList.length && this.r * this.r * this.clearPointsList.length > this.scale * this.totalArea) {
       this.show = true;
     }
     this.ctx.clearRect(x1, y1, this.r, this.r);
     this.ctx.draw(true);
   }
+  //操作手势
   bindTouch() {
     const _this = this;
     _this.page.onTouchStart = function (e) {
